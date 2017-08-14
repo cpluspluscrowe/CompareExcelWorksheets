12fde2416d3fb4b107f2946e621cf8461206d945
cpluspluscrowe <ccrowe@unomaha.edu>
2017-08-14 13:52:02 -0500

    Poi cell reference is 0 based, Excel is 1 based.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,7 @@ import org.apache.poi.openxml4j.opc.OPCPackage
 import org.apache.poi.ss.usermodel.DataFormatter
 import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
 
-import scala.collection.mutable.Map
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, Map}
 import java.io.File
 
 import scala.collection.mutable
@@ -36,6 +35,9 @@ object Main {
   def GetNextConstructionActivity(currentCa:String,i:Int,sheet:XSSFSheet): String = {
     val colACellRef: CellReference = new CellReference("A" + i.toString);
     val row = sheet.getRow(i);
+    if(row == null){
+      return currentCa;
+    }
     var cell = row.getCell(colACellRef.getCol);
     if (cell == null) {
       cell = row.createCell(colACellRef.getCol);
@@ -49,12 +51,14 @@ object Main {
     }
   }
   def IsTogAtRow(sheet:XSSFSheet,i:Int,togColumnLetter:String): Tuple2[Boolean,String] ={
-    val colACellRef: CellReference = new CellReference(togColumnLetter + i.toString);
-    val row = sheet.getRow(i);
     val colCCellRef:CellReference = new CellReference(togColumnLetter + i.toString);
+    val row = sheet.getRow(colCCellRef.getRow);
+    if(row == null){
+      return new Tuple2(false,null);
+    }
     var togCell = row.getCell(colCCellRef.getCol);
     if(togCell == null){
-      togCell = row.createCell(colACellRef.getCol);
+      togCell = row.createCell(colCCellRef.getCol);
     }
     val formatter = new DataFormatter
     val togValue = formatter.formatCellValue(togCell);
@@ -67,7 +71,7 @@ object Main {
     val facility = new Facility();
     val sheet:XSSFSheet = workbook.getSheet(worksheetName);
     var constructionActivity:String = null;
-    for(i <- 1.to(sheet.getLastRowNum)){
+    for(i <- 2.to(sheet.getLastRowNum)){
       constructionActivity = GetNextConstructionActivity(constructionActivity,i,sheet);
       if(!facility.map.contains(constructionActivity)){
         facility.map += (constructionActivity -> new ArrayBuffer[String]())
@@ -91,6 +95,19 @@ object Main {
     val formattedCellValue = formatter.formatCellValue(cell)
     return formattedCellValue;
   }
+  def CompareTogs(togs:Map[String,ArrayBuffer[String]],combined:Map[String,ArrayBuffer[String]]): Unit ={
+    for((k,v) <- togs){
+      if(!combined.contains(k)){
+        println("Combined missing: " + k);
+      }else{
+        for(tog <- togs(k)){
+          if(!combined(k).contains(tog)){
+            println("Combined Ca: " + k + " missing Tog: " + tog);
+          }
+        }
+      }
+    }
+  }
   def OpenExcel(): Unit ={
     val folderPath = "C:\\Create_Workbooks\\New Workbooks";
     val files:Array[File] = new File(folderPath).listFiles();
@@ -101,22 +118,9 @@ object Main {
         val name = GetName(workbook);
         val togs = GetTogs(workbook,"TOGS","C");
         val drawTogs = GetTogs(workbook,"Drawings and TOGS","E");
+        CompareTogs(togs.map,drawTogs.map);
+        CompareTogs(drawTogs.map,togs.map);
       }
     }
-    /*
-    val workbook:XSSFWorkbook = new XSSFWorkbook(OPCPackage.open(changesPath));
-    val sheet:XSSFSheet = workbook.getSheet("Added_to_SCoE");
-    var facilities:ArrayBuffer[String] = ArrayBuffer[String]();
-    for(i <- 5.to(43)){
-      var cellRef:CellReference = new CellReference("A" + i.toString);
-      var row = sheet.getRow(cellRef.getRow());
-      var cell = row.getCell(cellRef.getCol);
-      val formatter = new DataFormatter
-      val formattedCellValue = formatter.formatCellValue(cell)
-      if(formattedCellValue != null){
-        facilities += formattedCellValue;
-      }
-    }
-    workbook.close();*/
   }
 }
